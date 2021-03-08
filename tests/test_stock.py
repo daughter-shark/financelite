5a813c22ddd71c6454480863b719f6d057b150e4
d-aughter <sagongrin.1@gmail.com>
2021-03-07 21:10:54 -0600

    added pytests

---
+++ tests/test_stock.py
@@ -0,0 +1,28 @@
+from financelite import Stock, DataRequestException
+import pytest
+
+stock = Stock("gme")
+
+
+def test_chart():
+    stock_chart = stock.get_chart(interval="1d", range="5d")
+    assert stock_chart
+
+
+def test_chart_wrong_interval_range():
+    with pytest.raises(DataRequestException):
+        stock.get_chart("ff", "5d")
+    with pytest.raises(DataRequestException):
+        stock.get_chart("1d", "ff")
+    with pytest.raises(DataRequestException):
+        stock.get_chart("1d", "ff")
+
+
+def test_get_live():
+    assert stock.get_live()
+    with pytest.raises(DataRequestException):
+        Stock("somethingdumb").get_live()
+
+
+def test_stock_str():
+    assert str(stock) == "gme"
